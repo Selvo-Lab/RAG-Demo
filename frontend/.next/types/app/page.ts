816b9c6e7384a23b2eaae09d616cc4c606f3e894
Rade Petrovic <39063708+Rade1210@users.noreply.github.com>
2025-11-26 17:37:19 +0100

    Initial working version

--- frontend/.next/types/app/page.ts
+++ frontend/.next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /home/selvo-lab/Desktop/RAG-Demo/frontend/app/page.tsx
+// File: C:\Users\radep\OneDrive\Desktop\RAG-DEMO\frontend\app\page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
